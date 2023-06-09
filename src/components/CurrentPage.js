2bece284685af6f8ddbae781a43a9b5f5e9d9b3d
Alyssa Geria <alyssageria@gmail.com>
2023-06-09 14:26:16 -0700

    started render current page function and navbar functionality

--- src/components/CurrentPage.js
+++ src/components/CurrentPage.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
+import Resume from './pages/Resume';
 
 function CurrentPage() {
     const [currentPage, setCurrentPage] = useState('About');
@@ -17,8 +18,20 @@ function CurrentPage() {
         if (currentPage === 'Projects') {
             return <Projects />;
         }
+        if (currentPage === 'Resume') {
+            return <Resume />
+        }
 
     };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
 }
 
 export default CurrentPage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Navbar() {
+function Navbar({ currentPage, handlePageChange }) {
     return (
         <div>
             <div className='navbars'>
                 <h1 className='nav-title'>Alyssa Geria</h1>
                 <ul className='nav-items'>
-                    <li className='nav-item'><a href='#about-me'>About Me</a></li>
+                    <li className='nav-item'><a href='#about'>About Me</a></li>
                     <li className='nav-item'>Projects</li>
                     <li className='nav-item'>Contact</li>
                     <li className='nav-item'>Resume</li>
