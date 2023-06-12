579560ad96db92fe00dad4d66cff81898ab749f6
Alyssa Geria <alyssageria@gmail.com>
2023-06-12 13:54:22 -0700

    contact form working

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 
 function Contact() {
+    const validateEmail = (email) => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name') {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+        console.log(e.target.value)
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name) {
+            setErrorMessage('Please enter your name');
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            setErrorMessage('Please enter a valid email');
+            return;
+        }
+
+        if (!message) {
+            setErrorMessage('Please add a message')
+        }
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
     return (
         <div>
             <h1 className='is-size-2 has-text-white ml-6'>Contact Me</h1>
-            <form className='columns is-flex is-flex-direction-column mt-3'>
-                <div className='column is-half'>
+            <form className='columns is-flex is-flex-direction-column mt-3' id='contact-form'>
+                <div className='column is-two-thirds'>
                     <p className='has-text-white is-size-3 ml-6'>Name:</p>
-                    <input className='input custom-input has-background-black has-text-white ml-6' type='text' placeholder='Name'></input>
+                    <input className='input custom-input has-background-black has-text-white ml-6' type='text' placeholder='Name' name='name' value={name} onChange={handleInputChange}></input>
                 </div>
-                <div className='column is-half'>
+                <div className='column is-two-thirds'>
                     <p className='has-text-white is-size-3 ml-6'>Email:</p>
-                    <input className='input custom-input has-background-black has-text-white ml-6' type='text' placeholder='Email'></input>
+                    <input className='input custom-input has-background-black has-text-white ml-6' type='text' placeholder='Email' name='email' value={email} onChange={handleInputChange}></input>
                 </div>
                 <div>
-                    <div className='column is-half'>
+                    <div className='column is-two-thirds'>
                         <p className='has-text-white is-size-3 ml-6'>Message:</p>
-                        <textarea className=' textarea custom-input has-background-black has-text-white ml-6' type='text' placeholder='Message'></textarea>
+                        <textarea className=' textarea custom-input has-background-black has-text-white ml-6' type='text' placeholder='Message' name='message' value={message} onChange={handleInputChange}></textarea>
                     </div>
                 </div>
+                <div className='column is-one-quarter'>
+                    <button className='button has-background-black has-border-white has-text-white ml-6' onClick={handleFormSubmit}>Submit</button>
+                </div>
             </form>
-            <button className='button has-background-black has-border-white has-text-white ml-6'>Submit</button>
+            {errorMessage && (
+                <div>
+                    <p className="error-text has-text-white ml-6">{errorMessage}</p>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,26 +1,78 @@
 import React from 'react';
 import 'bulma/css/bulma.css';
-
+import githubIcon from '../../images/github-icon.png';
+import wwwIcon from '../../images/www-icon.png';
 
 function Projects() {
+    const myProjects = [
+        {
+            id: 1,
+            title: 'Homehub',
+            deployedLink: 'https://home-hub-management.herokuapp.com/',
+            githubLink: 'https://home-hub-management.herokuapp.com/'
+        },
+        {
+            id: 2,
+            title: 'Go For Fitness',
+            deployedLink: 'https://jacobryanwillis.github.io/Your_Digital_Personal_Trainer/',
+            githubLink: 'https://github.com/JacobRyanWillis/Your_Digital_Personal_Trainer'
+        },
+        {
+            id: 3,
+            title: 'Tech Blog',
+            deployedLink: 'https://sheltered-brushlands-70464.herokuapp.com/',
+            githubLink: 'https://github.com/alyssageria/tech-blog-mvc'
+        },
+        {
+            id: 4,
+            title: 'Homehub',
+            deployedLink: 'https://home-hub-management.herokuapp.com/',
+            githubLink: 'https://home-hub-management.herokuapp.com/'
+        },
+        {
+            id: 5,
+            title: 'Homehub',
+            deployedLink: 'https://home-hub-management.herokuapp.com/',
+            githubLink: 'https://home-hub-management.herokuapp.com/'
+        },
+        {
+            id: 6,
+            title: 'Homehub',
+            deployedLink: 'https://home-hub-management.herokuapp.com/',
+            githubLink: 'https://home-hub-management.herokuapp.com/'
+        }
+    ]
     return (
-        <div className='projects'>
-            <a href="https://home-hub-management.herokuapp.com/"><div className='project-1 project-box'>
-
-            </div></a>
-            <a href="https://jacobryanwillis.github.io/Your_Digital_Personal_Trainer/"><div className='project-2 project-box'>
-
-            </div></a>
-            <a href="https://sheltered-brushlands-70464.herokuapp.com/"><div className='project-3 project-box'>
-
-            </div></a>
-            <div className='project-4 project-box'>
-
+        <div>
+            <div>
+                <h1 className='has-text-white is-size-3 has-text-centered'>My Projects</h1>
             </div>
-            <div className='project-5 project-box'>
+            <div className='projects'>
+                {myProjects.map(project => {
+                    return (<div key={project.id} className='project-box is-flex is-justify-content-center' id={`project-${project.id}`}>
+                        <a href={project.githubLink}><img className='icons github' src={githubIcon}></img></a>
+                        <a href={project.deployedLink}><img className='icons www' src={wwwIcon}></img></a>
+                    </div>)
+                })}
+                {/* <a href="https://home-hub-management.herokuapp.com/"><div className='project-1 project-box'>
 
+                </div></a>
+                <a href="https://jacobryanwillis.github.io/Your_Digital_Personal_Trainer/"><div className='project-2 project-box'>
+
+                </div></a>
+                <a href="https://sheltered-brushlands-70464.herokuapp.com/"><div className='project-3 project-box'>
+
+                </div></a>
+                <a href='https://note-taker-expressjs1.herokuapp.com/'><div className='project-4 project-box'>
+
+                </div></a>
+                <div className='project-5 project-box'>
+
+                </div>
+                <div className='project-6 project-box'>
+
+                </div> */}
             </div>
-
         </div>
     )
 }
